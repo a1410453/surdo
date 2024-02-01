0776c2d7830faf2a9d345204cc1d2a099ce43346
a1410453 <a1410453@gmail.com>
2024-02-02 01:00:54 +0600

    refactoring long function

--- surdo/Profile/ProfileTableViewHeader.swift
+++ surdo/Profile/ProfileTableViewHeader.swift
@@ -10,17 +10,8 @@ import UIKit
 class ProfileTableViewHeader: UITableViewHeaderFooterView {
     weak var delegate: TableViewHeaderDelegate?
     static let identifier = "ProfileTableViewHeader"
-    
-    private var leadingAnchors: [NSLayoutConstraint] = []
-    private var trailingAnchors: [NSLayoutConstraint] = []
-    
-    private let indicator: UIView = {
-        let view = UIView()
-        view.translatesAutoresizingMaskIntoConstraints = false
-        view.backgroundColor = AppColor.red.uiColor
-        return view
-    }()
-    
+
+    // MARK: UI
     var achievementsLabel: UILabel = {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
@@ -114,6 +105,7 @@ class ProfileTableViewHeader: UITableViewHeaderFooterView {
         return button
     }()
     
+    // MARK: Setup Views
     override init(reuseIdentifier: String?) {
         super.init(reuseIdentifier: reuseIdentifier)
         addSubview(profileAvatarImageView)
@@ -125,84 +117,121 @@ class ProfileTableViewHeader: UITableViewHeaderFooterView {
         addSubview(levelTextLabel)
         addSubview(levelCountLabel)
         addSubview(privacyPolicyButton)
-        addSubview(indicator)
         addSubview(achievementsLabel)
         configureConstraints()
     }
     
-    @objc private func didTapPrivacyPolicyButton() {
-        delegate?.didTapPrivacyPolicyButton()
+    required init?(coder: NSCoder) {
+        fatalError()
     }
     
-    // swiftlint: disable all
+    // MARK: Setup Constraints
     private func configureConstraints() {
+        configureProfileAvatarImageViewConstraints()
+        configureDisplayNameLabelConstraints()
+        configureUsernameLabelConstraints()
+        configureJoinDateImageViewConstraints()
+        configureJoinDateLabelConstraints()
+        configureFollowersTextLabelConstraints()
+        configureFollowersCountLabelConstraints()
+        configurePrivacyPolicyButtonConstraints()
+        configureAchievementsLabelConstraints()
+    }
+    
+    private func configureProfileAvatarImageViewConstraints() {
         let profileAvatarImageViewConstraints = [
             profileAvatarImageView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 20),
             profileAvatarImageView.topAnchor.constraint(equalTo: topAnchor, constant: 20),
             profileAvatarImageView.widthAnchor.constraint(equalToConstant: 80),
             profileAvatarImageView.heightAnchor.constraint(equalToConstant: 80)
         ]
-        
+        NSLayoutConstraint.activate(profileAvatarImageViewConstraints)
+    }
+    
+    private func configureDisplayNameLabelConstraints() {
         let displayNameLabelConstraints = [
             displayNameLabel.leadingAnchor.constraint(equalTo: profileAvatarImageView.leadingAnchor,
                                                       constant: 20),
             displayNameLabel.topAnchor.constraint(equalTo: profileAvatarImageView.bottomAnchor,
                                                   constant: 20)
         ]
+        NSLayoutConstraint.activate(displayNameLabelConstraints)
+    }
+    
+    private func configureUsernameLabelConstraints() {
         let usernameLabelConstraints = [
             usernameLabel.leadingAnchor.constraint(equalTo: displayNameLabel.leadingAnchor),
             usernameLabel.topAnchor.constraint(equalTo: displayNameLabel.bottomAnchor, constant: 5)
         ]
+        NSLayoutConstraint.activate(usernameLabelConstraints)
+    }
+    
+    private func configureUserBioLabelConstraints() {
         let userBioLabelConstraints = [
             userBioLabel.leadingAnchor.constraint(equalTo: displayNameLabel.leadingAnchor),
             userBioLabel.topAnchor.constraint(equalTo: usernameLabel.bottomAnchor, constant: 5)
         ]
+        NSLayoutConstraint.activate(userBioLabelConstraints)
+    }
+    
+    private func configureJoinDateImageViewConstraints() {
         let joinDateImageViewConstraints = [
             joinDateImageView.leadingAnchor.constraint(equalTo: displayNameLabel.leadingAnchor),
             joinDateImageView.topAnchor.constraint(equalTo: userBioLabel.bottomAnchor, constant: 5)
         ]
+        NSLayoutConstraint.activate(joinDateImageViewConstraints)
+    }
+    
+    private func configureJoinDateLabelConstraints() {
         let joinDateLabelConstraints = [
             joinDateLabel.leadingAnchor.constraint(equalTo: joinDateImageView.trailingAnchor, constant: 2),
             joinDateLabel.bottomAnchor.constraint(equalTo: joinDateImageView.bottomAnchor)
         ]
-        let followersCountLabelConstraints = [
-            levelCountLabel.leadingAnchor.constraint(equalTo: displayNameLabel.leadingAnchor),
-            levelCountLabel.topAnchor.constraint(equalTo: joinDateLabel.bottomAnchor, constant: 10)
-        ]
+        NSLayoutConstraint.activate(joinDateLabelConstraints)
+    }
+    
+    private func configureFollowersTextLabelConstraints() {
         let followersTextLabelConstraints = [
             levelTextLabel.leadingAnchor.constraint(equalTo: levelCountLabel.trailingAnchor,
                                                     constant: 2),
             levelTextLabel.topAnchor.constraint(equalTo: joinDateLabel.bottomAnchor,
                                                 constant: 10)
         ]
+        NSLayoutConstraint.activate(followersTextLabelConstraints)
+    }
+    
+    private func configureFollowersCountLabelConstraints() {
+        let followersCountLabelConstraints = [
+            levelCountLabel.leadingAnchor.constraint(equalTo: displayNameLabel.leadingAnchor),
+            levelCountLabel.topAnchor.constraint(equalTo: joinDateLabel.bottomAnchor, constant: 10)
+        ]
         
+        NSLayoutConstraint.activate(followersCountLabelConstraints)
+       
+    }
+    
+    private func configurePrivacyPolicyButtonConstraints() {
         let privacyPolicyButtonConstraints = [
             privacyPolicyButton.leadingAnchor.constraint(equalTo: displayNameLabel.leadingAnchor),
             privacyPolicyButton.topAnchor.constraint(equalTo: levelTextLabel.bottomAnchor,
                                                      constant: 10)
         ]
-        
+        NSLayoutConstraint.activate(privacyPolicyButtonConstraints)
+    }
+    
+    private func configureAchievementsLabelConstraints() {
         let achievementsLabelConstraints = [
             achievementsLabel.centerXAnchor.constraint(equalTo: centerXAnchor),
             achievementsLabel.topAnchor.constraint(equalTo: privacyPolicyButton.bottomAnchor, constant: 60)
         ]
-        
-        NSLayoutConstraint.activate(profileAvatarImageViewConstraints)
-        NSLayoutConstraint.activate(displayNameLabelConstraints)
-        NSLayoutConstraint.activate(usernameLabelConstraints)
-        NSLayoutConstraint.activate(userBioLabelConstraints)
-        NSLayoutConstraint.activate(joinDateImageViewConstraints)
-        NSLayoutConstraint.activate(joinDateLabelConstraints)
-        NSLayoutConstraint.activate(followersTextLabelConstraints)
-        NSLayoutConstraint.activate(followersCountLabelConstraints)
-        NSLayoutConstraint.activate(privacyPolicyButtonConstraints)
         NSLayoutConstraint.activate(achievementsLabelConstraints)
     }
-    // swiftlint: enable all
     
-    required init?(coder: NSCoder) {
-        fatalError()
+    // MARK: Actions
+    @objc private func didTapPrivacyPolicyButton() {
+        delegate?.didTapPrivacyPolicyButton()
     }
+    
 }
 
 protocol TableViewHeaderDelegate: AnyObject {
