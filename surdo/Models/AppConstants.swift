d07e0111087e83d35cb79b2302ac2e5c94b41055
a1410453 <a1410453@gmail.com>
2024-02-02 01:01:16 +0600

    removed force unwraps

--- surdo/Models/AppConstants.swift
+++ surdo/Models/AppConstants.swift
@@ -7,24 +7,29 @@
 
 import Foundation
 
-// swiftlint: disable all
 struct AppConstants {
     // MARK: Video
-    static let baseURL = "https://firebasestorage.googleapis.com/v0/b/surdo-e3bd9.appspot.com/o/alphabet_videos%2F"
+    static let baseURL = 
+    "https://firebasestorage.googleapis.com/v0/b/surdo-e3bd9.appspot.com/o/alphabet_videos%2F"
     static let tokenURL = ".mp4?alt=media"
     static func makeURL(middlePart: Int) -> URL {
         let urlString = "\(baseURL)\(middlePart)\(tokenURL)"
-        return URL(string: urlString)!
+        guard let url = URL(string: urlString) else {
+            fatalError("Invalid URL")
+        }
+        return url
     }
-    
  
     // MARK: Picture
-    static let basePictureURL = "https://firebasestorage.googleapis.com/v0/b/surdo-e3bd9.appspot.com/o/alphabet_pictures%2F"
+    static let basePictureURL = 
+    "https://firebasestorage.googleapis.com/v0/b/surdo-e3bd9.appspot.com/o/alphabet_pictures%2F"
     static let tokenPictureURL = ".png?alt=media"
     
     static func makePictureURL(middlePart: Int) -> URL {
         let urlString = "\(basePictureURL)\(middlePart)\(tokenPictureURL)"
-        return URL(string: urlString)!
+        guard let url = URL(string: urlString) else {
+            fatalError("Invalid URL")
+        }
+        return url
     }
 }
-// swiftlint: enable all
